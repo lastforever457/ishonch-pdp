f689eeb995719f637f5f2f4cccc38286e32d74a5
dilshod505 <vohidjonov505@gmail.com>
2024-12-27 17:54:06 +0500

    student-page

--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,5 +1,54 @@
+import PageLayout from "../layouts/page-layout.tsx";
+import MySegmented from "../components/my-segmented.tsx";
+import MyTable from "../components/my-table.tsx";
+
 const Students = () => {
-  return <div>Students</div>;
+  return <PageLayout title={"Students"} segmented={<MySegmented segmentedValues={[{value: "sdfsdgsd", key: "vsfvfd"}, {value: "dgsgd", key: "sdfsf", isPrimary: true}]} queryName={"studentsTab"}/>}>
+    <MyTable columns={
+      [{
+        key: "firstName",
+        title: "F.I.O",
+        dataIndex: "fio",
+      },
+      { key: "phone", title: "Phone", dataIndex: "phone" },
+      { key: "role", title: "Role", dataIndex: "role" }
+    ]} data={[
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd",
+      },
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd",
+      },
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd",
+      },
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd",
+      },
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd",
+      },
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd",
+      },
+      {
+        fio: "sdfsdgsd",
+        phone: "sdfsdgsd",
+        role: "sdfsdgsd"
+      }
+    ]} />  </PageLayout>;
 };
 
 export default Students;
