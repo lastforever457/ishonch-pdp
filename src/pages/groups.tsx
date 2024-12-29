0afe18760031cc038c51dbcc841e15bcf3e3b84e
dilshod505 <vohidjonov505@gmail.com>
2024-12-29 13:22:10 +0500

    home + group + student

--- src/pages/groups.tsx
+++ src/pages/groups.tsx
@@ -1,5 +1,59 @@
+import PageLayout from "../layouts/page-layout.tsx";
+import MySegmented from "../components/my-segmented.tsx";
+import { useTranslation } from "react-i18next";
+import MyTable from "../components/my-table.tsx";
+
 const Groups = () => {
-  return <div>Groups</div>;
+  const { t } = useTranslation();
+
+  return (
+    <PageLayout
+      title={t("groups.title")}
+      segmented={
+        <MySegmented
+          segmentedValues={[
+            { value: t("groups.active"), key: "active" },
+            { value: t("groups.archive"), key: "archive", isPrimary: true },
+          ]}
+          queryName={"groupsTab"}
+        />
+      }
+    >
+      <MyTable
+        columns={[
+          {
+            key: "name",
+            title: "Name",
+            dataIndex: "name",
+          },
+          { key: "phone", title: "Phone", dataIndex: "phone" },
+          { key: "role", title: "Role", dataIndex: "role" },
+        ]}
+        data={[
+          {
+            name: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+          {
+            name: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+          {
+            name: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+          {
+            name: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+        ]}
+      />
+    </PageLayout>
+  );
 };
 
 export default Groups;
--- src/pages/students.tsx
+++ src/pages/students.tsx
@@ -1,54 +1,64 @@
 import PageLayout from "../layouts/page-layout.tsx";
 import MySegmented from "../components/my-segmented.tsx";
 import MyTable from "../components/my-table.tsx";
+import { useTranslation } from "react-i18next";
+import { useEffect, useMemo, useState } from "react";
+import { filials, groups, users } from "../test-data.ts";
+import { useLocationParams } from "../hooks/use-location-params.tsx";
+import { useParams } from "react-router-dom";
+import { Form } from "antd";
 
 const Students = () => {
-  return <PageLayout title={"Students"} segmented={<MySegmented segmentedValues={[{value: "sdfsdgsd", key: "vsfvfd"}, {value: "dgsgd", key: "sdfsf", isPrimary: true}]} queryName={"studentsTab"}/>}>
-    <MyTable columns={
-      [{
-        key: "firstName",
-        title: "F.I.O",
-        dataIndex: "fio",
-      },
-      { key: "phone", title: "Phone", dataIndex: "phone" },
-      { key: "role", title: "Role", dataIndex: "role" }
-    ]} data={[
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd",
-      },
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd",
-      },
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd",
-      },
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd",
-      },
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd",
-      },
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd",
-      },
-      {
-        fio: "sdfsdgsd",
-        phone: "sdfsdgsd",
-        role: "sdfsdgsd"
+  const { t } = useTranslation();
+
+  return (
+    <PageLayout
+      title={t("students.title")}
+      segmented={
+        <MySegmented
+          segmentedValues={[
+            { value: t("students.students"), key: "students" },
+            { value: t("students.archive"), key: "archive", isPrimary: true },
+          ]}
+          queryName={"studentsTab"}
+        />
       }
-    ]} />  </PageLayout>;
+    >
+      <MyTable
+        columns={[
+          {
+            key: "firstName",
+            title: "F.I.O",
+            dataIndex: "fio",
+          },
+          { key: "phone", title: "Phone", dataIndex: "phone" },
+          { key: "role", title: "Role", dataIndex: "role" },
+        ]}
+        data={[
+          {
+            fio: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+          {
+            fio: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+          {
+            fio: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+          {
+            fio: "sdfsdgsd",
+            phone: "sdfsdgsd",
+            role: "sdfsdgsd",
+          },
+        ]}
+      />{" "}
+    </PageLayout>
+  );
 };
 
 export default Students;
