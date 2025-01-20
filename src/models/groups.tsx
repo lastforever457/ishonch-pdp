a4d65b6de9985c2ffa71411d6d4f7fb21b3bea83
dilshod505 <vohidjonov505@gmail.com>
2025-01-20 15:39:37 +0500

    fix

--- src/models/groups.tsx
+++ src/models/groups.tsx
@@ -1,15 +1,28 @@
-import { useQuery } from '@tanstack/react-query'
-import api from './axios'
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import api from "./axios";
 
-export type GroupType = 'ACTIVE' | 'ARCHIVE'
+export type GroupType = "ACTIVE" | "ARCHIVE";
 
 export const useGroups = (status: GroupType) => {
   const data = useQuery({
-    queryKey: ['groups'],
+    queryKey: ["groups"],
     queryFn: async () => {
-      const { data } = await api.get(`/group/${status}`)
-      return data
+      const { data } = await api.get(`/group/${status}`);
+      return data;
     },
-  })
-  return data
-}
+  });
+  return data;
+};
+
+export const useCreateGroup = () => {
+  const queryClient = useQueryClient();
+  const data = useMutation({
+    mutationKey: ["create-group"],
+    mutationFn: async (newGroup: Record<string, any>) => {
+      await api.post("/group/addGroup", newGroup);
+      queryClient.invalidateQueries({ queryKey: ["groups"] });
+    },
+  });
+
+  return data;
+};
--- src/pages/group/groups.tsx
+++ src/pages/group/groups.tsx
@@ -1,180 +1,242 @@
-import { useMutation } from '@tanstack/react-query'
-import { useMemo } from 'react'
-import { useTranslation } from 'react-i18next'
-import { AutoForm } from '../../components/auto-form.tsx'
-import MyDrawer from '../../components/my-drawer.tsx'
-import MySegmented from '../../components/my-segmented.tsx'
-import MyTable from '../../components/my-table.tsx'
-import { useLocationParams } from '../../hooks/use-location-params.tsx'
-import PageLayout from '../../layouts/page-layout.tsx'
-import api from '../../models/axios.ts'
+import { useMutation } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { AutoForm, FormField } from "../../components/auto-form.tsx";
+import MyDrawer from "../../components/my-drawer.tsx";
+import MySegmented from "../../components/my-segmented.tsx";
+import MyTable from "../../components/my-table.tsx";
+import { useLocationParams } from "../../hooks/use-location-params.tsx";
+import PageLayout from "../../layouts/page-layout.tsx";
+import api from "../../models/axios.ts";
+import Attendance from "./attendance.tsx";
+import { useUsers } from "../../models/users.tsx";
+import { useCreateGroup, useGroups } from "../../models/groups.tsx";
+import { useRooms } from "../../models/rooms.tsx";
+import { values } from "lodash-es";
 
 const Groups = () => {
-  const { t } = useTranslation()
-  const { query } = useLocationParams()
-  const { mutate } = useMutation({
-    mutationKey: ['create-group'],
-    mutationFn: async (newGroup: Record<string, any>) => {
-      await api.post('/group/create', newGroup)
-    },
-  })
+  const { t } = useTranslation();
+  const { query } = useLocationParams();
+  const { data: groups, isLoading: isGroupsLoading } = useGroups("ACTIVE");
+  const { data: rooms } = useRooms();
+  const { mutate } = useCreateGroup();
 
-  const getTeachers = async () => {
-    const { data } = await api.get('/teachers')
-    console.log(data)
-  }
+  const { data: teachers, isLoading: isTeacherLoading } = useUsers("TEACHER");
 
   const fields = useMemo(
     () => [
       {
-        name: 'courseName',
-        label: t('groups.courseName'),
-        type: 'input',
+        name: "groupName",
+        label: t("groups.name"),
+        type: "input",
         rules: [
           {
             required: true,
-            message: t('formMessages.firstName'),
+            message: t("formMessages.firstName"),
           },
         ],
       },
       {
-        name: 'teacher',
-        label: t('groups.chooseTeacher'),
-        type: 'select',
+        name: "courseName",
+        label: t("groups.courseName"),
+        type: "input",
         rules: [
           {
             required: true,
-            message: t('formMessages.phone'),
+            message: t("formMessages.firstName"),
           },
         ],
       },
       {
-        name: 'days',
-        label: t('groups.days'),
-        type: 'select',
+        name: "teacherId",
+        label: t("groups.chooseTeacher"),
+        type: "select",
+        options: teachers?.data?.map((teacher: Record<string, any>) => ({
+          label: teacher.firstname + " " + teacher.lastname,
+          value: teacher.id,
+        })),
+        rules: [
+          {
+            required: true,
+            message: t("formMessages.phone"),
+          },
+        ],
+      },
+      {
+        name: "days",
+        label: t("groups.days"),
+        mode: "multiple",
+        type: "select",
         options: [
           {
-            label: t('groups.monday'),
-            value: 'Dushanba',
+            label: t("groups.monday"),
+            value: "Dushanba",
           },
           {
-            label: t('groups.tuesday'),
-            value: 'Seshanba',
+            label: t("groups.tuesday"),
+            value: "Seshanba",
           },
           {
-            label: t('groups.wednesday'),
-            value: 'Chorshanba',
+            label: t("groups.wednesday"),
+            value: "Chorshanba",
           },
           {
-            label: t('groups.thursday'),
-            value: 'Payshanba',
+            label: t("groups.thursday"),
+            value: "Payshanba",
           },
           {
-            label: t('groups.friday'),
-            value: 'Juma',
+            label: t("groups.friday"),
+            value: "Juma",
           },
           {
-            label: t('groups.saturday'),
-            value: 'Shanba',
+            label: t("groups.saturday"),
+            value: "Shanba",
           },
         ],
         rules: [
           {
             required: true,
-            message: t('formMessages.role'),
-          },
-        ],
-      },
-      {
-        name: 'rooms',
-        label: t('groups.chooseRoom'),
-        type: 'select',
-        rules: [
-          {
-            required: true,
-            message: t('formMessages.role'),
+            message: t("formMessages.role"),
           },
         ],
       },
       {
-        name: 'time',
-        label: t('groups.startTime'),
-        type: 'timepicker',
+        name: "roomId",
+        label: t("groups.chooseRoom"),
+        type: "select",
+        options: rooms?.data?.map((room: Record<string, any>) => ({
+          label: room?.name,
+          value: room.id,
+        })),
         rules: [
           {
             required: true,
-            message: t('formMessages.timeStart'),
+            message: t("formMessages.role"),
+          },
+        ],
+      },
+      {
+        name: "startTime",
+        label: t("groups.startTime"),
+        type: "timepicker",
+        rules: [
+          {
+            required: true,
+            message: t("formMessages.timeStart"),
+          },
+        ],
+      },
+      {
+        name: "startDate",
+        label: t("groups.startDate"),
+        type: "datepicker",
+        rules: [
+          {
+            required: true,
+            message: t("formMessages.startDate"),
+          },
+        ],
+      },
+      {
+        name: "endDate",
+        label: t("groups.endDate"),
+        type: "datepicker",
+        rules: [
+          {
+            required: true,
+            message: t("formMessages.endDate"),
+          },
+        ],
+      },
+      {
+        name: "groupPrice",
+        label: t("groups.price"),
+        type: "input",
+        rules: [
+          {
+            required: true,
+            message: t("formMessages.endDate"),
           },
         ],
       },
     ],
-    [t]
-  )
+    [t, teachers]
+  );
 
   const columns = useMemo(
     () => [
       {
-        key: 'group',
-        title: t('groups.title'),
-        dataIndex: 'Guruhlar',
+        key: "group",
+        title: t("groups.title"),
+        dataIndex: "Guruhlar",
       },
       {
-        key: 'teacher',
-        title: t('groups.chooseTeacher'),
-        dataIndex: 'Teacher',
+        key: "teacher",
+        title: t("groups.chooseTeacher"),
+        dataIndex: "Teacher",
       },
 
       {
-        key: 'st number',
-        title: t('groups.stNumber'),
-        dataIndex: 'St number',
+        key: "st number",
+        title: t("groups.stNumber"),
+        dataIndex: "St number",
       },
       {},
       {
-        key: 'days',
-        title: t('groups.days'),
-        dataIndex: 'Kunlar',
+        key: "days",
+        title: t("groups.days"),
+        dataIndex: "Kunlar",
       },
       {
-        key: 'time',
-        title: t('groups.startTime'),
-        dataIndex: 'Vaqt',
+        key: "time",
+        title: t("groups.startTime"),
+        dataIndex: "Vaqt",
       },
     ],
     [t]
-  )
+  );
 
-  const onFinish = () => {
-    mutate({})
+  const onFinish = (values: Record<string, any>) => {
+    console.log(values);
+    mutate({
+      ...values,
+      groupPrice: Number(values.groupPrice),
+    });
+  };
+
+  if (isGroupsLoading || isTeacherLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div>Loading...</div>
+      </div>
+    );
   }
 
   return (
     <PageLayout
-      title={t('groups.title')}
+      title={t("groups.title")}
       segmented={
         <MySegmented
           segmentedValues={[
-            { value: t('groups.active'), key: 'active' },
-            { value: t('groups.archive'), key: 'archive', isPrimary: true },
+            { value: t("groups.active"), key: "active" },
+            { value: t("groups.archive"), key: "archive", isPrimary: true },
+            { value: t("groups.attendance"), key: "attendance" },
           ]}
-          queryName={'groupsTab'}
+          queryName={"groupsTab"}
         />
       }
     >
-      <MyTable name="group" columns={columns} data={[]} />
-      <MyDrawer entryPoint="add" title={t('groups.titleSingular')}>
+      <MyDrawer entryPoint="add" title={t("groups.titleSingular")}>
         <AutoForm
-          fields={fields}
+          fields={fields as FormField[]}
           onCancel={() => {}}
-          onFinish={() => {
-            onFinish()
-          }}
-          saveTitle={t('crud.create')}
-          cancelTitle={t('form.cancel')}
+          onFinish={onFinish}
+          saveTitle={t("crud.create")}
+          cancelTitle={t("form.cancel")}
         />
       </MyDrawer>
     </PageLayout>
-  )
-}
+  );
+};
 
-export default Groups
+export default Groups;
