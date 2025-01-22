efaba149d09063543faf4da017e767d3fcc6a49b
lastforever457 <lastforever457@gmail.com>
2025-01-22 18:44:29 +0500

    fixed

--- src/pages/finance/debtors.tsx
+++ src/pages/finance/debtors.tsx
@@ -1,60 +1,60 @@
-import { useMemo } from "react";
-import { useTranslation } from "react-i18next";
-import MyTable from "../../components/my-table";
-import { debtors } from "../../test-data";
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import MyTable from '../../components/my-table'
+import { debtors } from '../../test-data'
 
 const Debtors = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   const columns = useMemo(
     () => [
       {
-        title: `${t("form.name")} ${t("form.lastName")}`,
-        dataIndex: "fullName",
-        key: "fullName",
-        fixed: true
+        title: `${t('form.name')} ${t('form.lastName')}`,
+        dataIndex: 'fullName',
+        key: 'fullName',
+        fixed: true,
       },
       {
-        title: t("form.phone"),
-        dataIndex: "phone",
-        key: "phone",
+        title: t('form.phone'),
+        dataIndex: 'phone',
+        key: 'phone',
         ellipsis: {
           showTitle: false,
         },
       },
       {
-        title: t("groups.title"),
-        key: "group",
+        title: t('groups.title'),
+        key: 'group',
         render: (_: any, record: Record<string, any>) => (
           <span>
             <span
               style={{
-                backgroundColor: "#A0D468",
-                color: "white",
-                padding: "4px 8px",
-                borderRadius: "4px",
+                backgroundColor: '#A0D468',
+                color: 'white',
+                padding: '4px 8px',
+                borderRadius: '4px',
               }}
             >
               {record.name}
-            </span>{" "}
+            </span>{' '}
             {record.time}
           </span>
         ),
       },
       {
-        title: t("form.comment"),
-        dataIndex: "comment",
-        key: "comment",
+        title: t('form.comment'),
+        dataIndex: 'comment',
+        key: 'comment',
       },
     ],
     [t]
-  );
+  )
 
   return (
     <>
-      <MyTable columns={columns} data={debtors} />
+      <MyTable name="finance" columns={columns} data={debtors} />
     </>
-  );
-};
+  )
+}
 
-export default Debtors;
+export default Debtors
