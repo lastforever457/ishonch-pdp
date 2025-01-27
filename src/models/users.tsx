4ccb06a03d3ff9931859d0be016d92b44ca1be84
lastforever457 <lastforever457@gmail.com>
2025-01-27 18:37:52 +0500

    fix some changes

--- src/models/users.tsx
+++ src/models/users.tsx
@@ -1,79 +1,76 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import api from "./axios";
-import { message } from "antd";
-import { t } from "i18next";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { message } from 'antd'
+import { t } from 'i18next'
+import api from './axios'
 
-export type StaffTypes = "TEACHER" | "OTHER" | "CLEANER";
+export type StaffTypes = 'TEACHER' | 'OTHER' | 'CLEANER'
 
 export interface DataType {
-  message: string;
-  data: Record<string, any>;
-  status: boolean;
+  message: string
+  data: Record<string, any>
+  status: boolean
 }
 
 export const useUsers = (status: StaffTypes) => {
   const users = useQuery({
-    queryKey: ["users"],
+    queryKey: ['users'],
     queryFn: async () => {
-      const res = await api.get(`/staff/${status}`);
-      message.success(t("formMessages.success"));
-      return res.data;
+      const res = await api.get(`/staff/${status}`)
+      return res.data
     },
-  });
+  })
 
-  return users;
-};
+  return users
+}
 
 export const useUser = (id: number | string) => {
   const user = useQuery({
-    queryKey: ["user"],
+    queryKey: ['user'],
     queryFn: async () => {
-      if (!id) return;
-      const res = await api.get(`/staff/profile/${id}`);
-      message.success(t("formMessages.success"));
-      return res.data;
+      const res = await api.get(`/staff/profile/${id}`)
+      return res.data
     },
     retry: false,
     select: (data) => data?.data,
-  });
-  return user;
-};
+  })
+  return user
+}
 
 export const useCreateUser = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   const createUser = useMutation({
-    mutationKey: ["create-user"],
+    mutationKey: ['create-user'],
     mutationFn: async (newUser: Record<string, any>) => {
-      await api.post("/staff/create", newUser);
-      message.success(t("formMessages.success"));
-      await queryClient.invalidateQueries({ queryKey: ["users"] });
+      await api.post('/staff/create', newUser)
+      message.success(t('formMessages.success'))
+      await queryClient.invalidateQueries({ queryKey: ['users'] })
     },
-  });
-  return createUser;
-};
+  })
+  return createUser
+}
 
 export const useUpdateUser = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   const updateUser = useMutation({
-    mutationKey: ["update-user"],
+    mutationKey: ['update-user'],
     mutationFn: async (user: Record<string, any>) => {
-      await api.patch(`/staff/${user.id}`, user.data);
-      message.success(t("formMessages.success"));
-      await queryClient.invalidateQueries({ queryKey: ["users"] });
+      await api.patch(`/staff/${user.id}`, user.data)
+      message.success(t('formMessages.success'))
+      await queryClient.invalidateQueries({ queryKey: ['users'] })
     },
-  });
-  return updateUser;
-};
+  })
+  return updateUser
+}
 
 export const useDeleteUser = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   const deleteUser = useMutation({
-    mutationKey: ["delete-user"],
+    mutationKey: ['delete-user'],
     mutationFn: async (id: number | string) => {
-      await api.delete(`/staff/${id}`);
-      message.success(t("formMessages.success"));
-      await queryClient.invalidateQueries({ queryKey: ["users"] });
+      await api.delete(`/staff/${id}`)
+      message.success(t('formMessages.success'))
+      await queryClient.invalidateQueries({ queryKey: ['users'] })
     },
-  });
-  return deleteUser;
-};
+  })
+  return deleteUser
+}
