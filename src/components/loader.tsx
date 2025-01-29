79eefba73ca2490e5cec282c7026193367bb42ea
lastforever457 <lastforever457@gmail.com>
2025-01-29 19:07:13 +0500

    fix loader

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,9 +1,25 @@
-import { Riple } from 'react-loading-indicators'
+import { CSSProperties } from 'react';
+import RingLoader from 'react-spinners/RingLoader';
 
-export const Loader = () => {
+const override: CSSProperties = {
+  display: 'block',
+  margin: '0 auto',
+  borderColor: 'red',
+};
+
+export const CustomLoader = () => {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-80px)] lg:h-[calc(100vh-100px)]">
-      <Riple color="#32cd32" size="large" text="" textColor="" />
+    <div className="flex h-[calc(100vh-80px)] items-center justify-center lg:h-[calc(100vh-100px)]">
+      <span>
+        <RingLoader
+          color={'var(--theme-color)'}
+          cssOverride={override}
+          size={80}
+          className="text-primary-blue"
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </span>
     </div>
-  )
-}
+  );
+};
